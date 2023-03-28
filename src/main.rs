a45b08fafe1425efc8e9b96455f9c9ad397e9d2e
Mathias Storm <mathias@storm.lc>
2023-03-28 19:58:49 +0200

    just a simple rust number guesser :)

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+fn get_random_number() -> i8 {
+    let target: i8 = rand::random();
+    target.abs().into()
+}
+fn main() {
+    println!("Welcome to the EPIC number guesser! Guess a number, will you?");
+
+    let target = get_random_number();
+
+    println!("{}", target);
+
+    loop {
+        let mut buf = String::new();
+        match std::io::stdin().read_line(&mut buf) {
+            Ok(_) => {}
+            _ => panic!("error while printing!"),
+        }
+        let input = buf.trim();
+
+        let num_guess = match input.parse::<i32>() {
+            Ok(n) => n,
+            _ => 0,
+        };
+
+        if num_guess == target.into() {
+            break;
+        } else {
+            println!("Ah bummer, thats wrong! :( Try again!")
+        }
+    }
+
+    println!("You found it! It was {} :)", target)
+}
